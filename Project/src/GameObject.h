60b088bce3f0aa05570db10d33e8e26092ab84bd
Torbjornsson <david.torbjornsson@gmail.com>
2019-02-17 10:35:25 +0100

    Game object from lab 4

--- Project/src/GameObject.h
+++ Project/src/GameObject.h
@@ -1,11 +1,27 @@
 #include <vector>
 
-class inputComponent;
+class Component;
 
 class GameObject{
-    private:
+    protected:
         std::vector<GameObject*> recievers;
+        std::vector<Component*> components;
         
     public:
+        double horizontalPos;
+        double verticalPos;
 
-}
+        bool enabled;
+
+        ~virtual GameObject();
+
+        virtual void Create();
+        virtual void AddComponent(Component * component);
+
+        virtual void Init();
+        virtual void Update(float dt);
+        virtual void Destroy();
+        virtual void AddReceiver(GameObject * gameObject);
+        virtual void Receive(Message m){}
+        void Send(Message m);
+};
